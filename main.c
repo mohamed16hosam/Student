75f5c4f963ecc03a98d94cb0a004ae023c18de7b
unknown <you@example.com>
2021-04-17 23:26:41 +0200

    This is our project

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+char stname [20] ; //character array for student name //
+int stid ; //student id //
+int stbirthday ; //student birthday //
+int stbirthmon ; //student birth month //
+int stbirthyear ; //student birth year //
+int stscore ; //student score of last year //
+};
+int main()
+{
+    printf("Welcome to our program\n");
+
+    return 0;
+}
